b3c89b8377fceb070e262e6e8a76f5ae1d146c6e
SyedRiyazUddin1 <59564957+SyedRiyazUddin1@users.noreply.github.com>
2020-06-03 22:35:21 +0530

    Add files via upload

--- MySpringBootApplication/src/main/java/com/myspringboot/security/SecurityConfiguration.java
+++ MySpringBootApplication/src/main/java/com/myspringboot/security/SecurityConfiguration.java
@@ -1,45 +1,46 @@
 package com.myspringboot.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
-//class for JPA authentication configuration using H2 database
+//class for JDBC authentication configuration using MySQL DB
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
-	UserDetailsService userDetailsService;
+	private DataSource datasource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		// Setting up the configuration on the auth object
-		auth.userDetailsService(userDetailsService);
+		
+		// Setting up the JDBC configuration on the auth object
+		auth.jdbcAuthentication()
+		.dataSource(datasource)
+		.usersByUsernameQuery("select username, password, enabled "
+		        + "from users "
+				+"where username = ?")
+		.authoritiesByUsernameQuery("select username, role "
+				+ "from authorities "
+				+ "where username= ?");
+		
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	
-		http.authorizeRequests()  //authorize all requests
-		
-		.antMatchers("/myapp/viewProducts-page/**").hasRole("User1") 
-		.antMatchers("/myapp/viewWithoutEdit").hasAnyRole("User1","User2")
-		.antMatchers("/").permitAll()
-		.and().formLogin();
-		
+
+		http.authorizeRequests() // authorize all requests
+
+				.antMatchers("/myapp/viewProducts-page/**").hasRole("USER")
+				.antMatchers("/myapp/viewWithoutEdit").hasAnyRole("USER", "ADMIN")
+				.antMatchers("/").permitAll().and().formLogin();
+
 	}
-	
-	
-//moved this to Appconfig class
-//	@Bean
-//	public PasswordEncoder getPasswordEncorder() {
-//
-//		return NoOpPasswordEncoder.getInstance();
-//	}
 
 }
