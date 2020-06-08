4ea3c1122d7747175086e56c78a1e0d4c3cffb64
SyedRiyazUddin1 <59564957+SyedRiyazUddin1@users.noreply.github.com>
2020-06-08 23:30:17 +0530

    Add files via upload

---
+++ spring-security-jwt/src/main/java/com/springboot/service/MyUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.springboot.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return new User("Riyaz", "password", new ArrayList<>());
+	}
+
+}
