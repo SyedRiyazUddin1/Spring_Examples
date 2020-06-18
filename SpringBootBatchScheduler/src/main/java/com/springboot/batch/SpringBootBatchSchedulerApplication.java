81d4ad25f70f3ed130182b749b0bb3b085990d3f
SyedRiyazUddin1 <59564957+SyedRiyazUddin1@users.noreply.github.com>
2020-06-18 22:21:30 +0530

    Add files via upload

---
+++ SpringBootBatchScheduler/src/main/java/com/springboot/batch/SpringBootBatchSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchSchedulerApplication.class, args);
+	}
+
+}
---
+++ SpringBootBatchScheduler/src/test/java/com/springbootbatch/example/SpringBootBatchScheduler/SpringBootBatchSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootbatch.example.SpringBootBatchScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatchSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
