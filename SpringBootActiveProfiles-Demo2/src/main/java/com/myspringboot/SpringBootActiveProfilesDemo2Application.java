b458afd4b8529023437e1c96d8916fe0664a49bd
SyedRiyazUddin1 <59564957+SyedRiyazUddin1@users.noreply.github.com>
2020-08-05 22:38:38 +0530

    Add files via upload

---
+++ SpringBootActiveProfiles-Demo2/src/main/java/com/myspringboot/SpringBootActiveProfilesDemo2Application.java
@@ -0,0 +1,13 @@
+package com.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActiveProfilesDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActiveProfilesDemo2Application.class, args);
+	}
+
+}
